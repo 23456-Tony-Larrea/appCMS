302a682eca70bacff97bd1015dbe70b619b90681
23456-Tony-Larrea <avs.larrea@yavirac.edu.ec>
2022-12-11 19:35:26 -0500

    all proyect

---
+++ vendor/jeroennoten/laravel-adminlte/resources/lang/en/adminlte.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    'full_name'                   => 'Nombre completo',
+    'email'                       => 'Email',
+    'password'                    => 'Contraseña',
+    'retype_password'             => 'Repetir contraseña',
+    'remember_me'                 => 'Recordarme',
+    'register'                    => 'Registrarse',
+    'register_a_new_membership'   => 'Registrar un nuevo miembro',
+    'i_forgot_my_password'        => 'Olvidé mi contraseña',
+    'i_already_have_a_membership' => 'Ya tengo una membresía',
+    'sign_in'                     => 'Iniciar sesión',
+    'log_out'                     => 'Cerrar sesión',
+    'toggle_navigation'           => 'Toggle navigation',
+    'login_message'               => 'Ingresa tus datos para iniciar sesión',
+    'register_message'            => 'Registrar un nuevo miembro',
+    'password_reset_message'      => 'Restablecer contraseña',
+    'reset_password'              => 'Restablecer contraseña',
+    'send_password_reset_link'    => 'Enviar enlace de restablecimiento de contraseña',
+    'verify_message'              => 'Tu cuenta necesita una verificación',
+    'verify_email_sent'           => 'A fresh verification link has been sent to your email address.',
+    'verify_check_your_email'     => 'Before proceeding, please check your email for a verification link.',
+    'verify_if_not_recieved'      => 'If you did not receive the email',
+    'verify_request_another'      => 'click here to request another',
+    'confirm_password_message'    => 'Please, confirm your password to continue.',
+];
